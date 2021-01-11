359582f82d9c1d49276bde55987266e46c5666e2
AndreiSecu1997 <36158416+AndreiSecu1997@users.noreply.github.com>
2021-01-11 18:32:57 +0200

    First Commit
    Added Selenium Topic lesson

---
+++ Automation Java/Selenium Topic/src/main/java/AsecuTests.java
@@ -0,0 +1,59 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class AsecuTests {
+    @Test
+    public static void aboutUsPageIsAvailable() throws InterruptedException {
+
+        //Adjust the path to your webDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\andrei.secu\\Desktop\\Amdaris%20Test%20Automation%20Course%202020\\Automation Java\\Selenium Topic\\src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.phptravels.net/");
+
+        WebElement companyDropDown = driver.findElement(By.xpath("//*[@id=\"mobileMenuMain\"]/nav/ul[2]/li/a"));
+        WebElement contactUsButton = driver.findElement(By.xpath("//*[@id=\"mobileMenuMain\"]/nav/ul[2]/li/ul/li[2]/a"));
+
+        companyDropDown.click();
+        Thread.sleep(2500);
+        contactUsButton.click();
+
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://www.phptravels.net/about-Us";
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public static void aboutUsPageIsNotAvailable() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "Automation Java/Selenium Topic/src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.phptravels.net/");
+
+        WebElement companyDropDown = driver.findElement(By.xpath("//*[@id=\"mobileMenuMain\"]/nav/ul[2]/li/a"));
+        WebElement contactUsButton = driver.findElement(By.xpath("//*[@id=\"mobileMenuMain\"]/nav/ul[2]/li/ul/li[2]/a"));
+
+        companyDropDown.click();
+        Thread.sleep(2500);
+        contactUsButton.click();
+
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://www.phptravels.net/about-Us";
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        driver.close();
+        driver.quit();
+    }
+}
